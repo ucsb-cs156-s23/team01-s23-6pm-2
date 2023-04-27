077a70dea84a77d3858636bc329e514f49327fdc
Anand Giduthuri <agiduthuri@ucsb.edu>
2023-04-27 16:29:15 -0700

    Add fixtures for game objects (#14)

---
+++ frontend/src/fixtures/gamesFixtures.js
@@ -0,0 +1,46 @@
+const gamesFixtures = {
+    oneGame:
+    [
+      {
+        "id": 1,
+        "name": "Super Mario Bros.",
+        "developer": "Nintendo",
+        "publisher": "Nintendo",
+        "year": "1985",
+        "rating": "E"
+      }
+    ],
+
+    threeGames:
+    [
+      {
+        "id": 2,
+        "name": "Doom",
+        "developer": "id Software",
+        "publisher": "id Software",
+        "year": "1993",
+        "rating": "M"
+      },
+        
+      {
+        "id": 3,
+        "name": "Sonic Unleashed",
+        "developer": "Sonic Team",
+        "publisher": "Sega",
+        "year": "2008",
+        "rating": "E10+"
+      },
+
+      {
+        "id": 4,
+        "name": "Ratchet & Clank",
+        "developer": "Insomniac Games",
+        "publisher": "Sony Computer Entertainment",
+        "year": "2002",
+        "rating": "T"
+      },
+ 
+    ]
+};
+
+export { gamesFixtures };
