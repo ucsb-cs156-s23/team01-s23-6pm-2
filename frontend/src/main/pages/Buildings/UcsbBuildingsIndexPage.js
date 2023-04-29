8098de8c55f6c60db4d64627c9d890c3daf8a706
jgarcia-ucsb <114465206+jgarcia-ucsb@users.noreply.github.com>
2023-04-28 19:48:05 -0700

    Added UcsbBuildings Index page and test (#51)

---
+++ frontend/src/main/pages/Buildings/UcsbBuildingsIndexPage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UcsbBuildingsTable from 'main/components/Buildings/UcsbBuildingsTable';
+import { ucsbBuildingsUtils } from 'main/utils/ucsbBuildingsUtils';
+import { useNavigate, Link } from 'react-router-dom';
+
+export default function UcsbBuildingsIndexPage() {
+
+    const navigate = useNavigate();
+
+    const ucsbBuildingsCollection = ucsbBuildingsUtils.get();
+    const ucsbbuildings = ucsbBuildingsCollection.ucsbbuildings;
+
+    const showCell = (cell) => JSON.stringify(cell.row.values);
+
+    const deleteCallback = async (cell) => {
+        console.log(`UcsbBuildingsIndexPage deleteCallback: ${showCell(cell)})`);
+        ucsbBuildingsUtils.del(cell.row.values.id);
+        navigate("/buildings");
+    }
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                <Button style={{ float: "right" }} as={Link} to="/buildings/create">
+                    Create Ucsb Building
+                </Button>
+                <h1>Ucsb Buildings</h1>
+                <UcsbBuildingsTable ucsbbuildings={ucsbbuildings} deleteCallback={deleteCallback} />
+            </div>
+        </BasicLayout>
+    )
+}
